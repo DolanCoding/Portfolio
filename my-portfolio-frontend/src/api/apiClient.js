3c490a0a94fa10dfa00fcc17dd4df47f38d8e571
DolanCoding <saschafischer0695@gmail.com>
2025-05-10 21:14:56 +0200

    changing server to the onrender server

--- my-portfolio-frontend/src/api/apiClient.js
+++ my-portfolio-frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://localhost:3001",
+  baseURL: "https://portfolio-backend-qu09.onrender.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
